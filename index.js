6c03bb167bb4fe7853f58f4a1064d0a03056c231
bsao17 <declic62@gmail.com>
2022-03-18 17:49:32 +0100

    Add Nodemon and Cors

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
+const cors = require('cors');
 const env = require('dotenv').config();
 
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hello express.js 👋")
+})
 
 app.listen(port, () => {
     console.log("Server is running on port " + port);
